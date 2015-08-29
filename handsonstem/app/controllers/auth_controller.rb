8ccdd029e2ac4ca789e6cf82a06df1a59360fc92
Bianca Tamayo <bancie@gmail.com>
2015-08-29 00:12:10 -0700

    OAuth flow for Eventbrite

--- handsonstem/app/controllers/auth_controller.rb
+++ handsonstem/app/controllers/auth_controller.rb
@@ -1,15 +1,24 @@
 class AuthController < ApplicationController
   def eventbriteCallback
-    @code =  params[:code]
-    
-    uri = URI.parse("https://www.eventbrite.com/oauth/token?code=THE_USERS_AUTH_CODE&client_secret=YOUR_CLIENT_SECRET&client_id=YOUR_API_KEY&grant_type=authorization_code")
-    http = Net::HTTP.new(uri.host, nil)
+    code = params[:code]
+
+    uri = URI.parse("https://www.eventbrite.com/oauth/token?code=" + code + "&client_secret=QKAURIDJ2E37OVWZOXMJLAMUGISHXLTLQR2IOBNNNTWAAU53BD&client_id=LM35T2HNQKLMFV5WVQ&grant_type=authorization_code")
+    http = Net::HTTP.new(uri.host, 443)
     http.use_ssl = true
     http.verify_mode = OpenSSL::SSL::VERIFY_NONE
-    request = Net::HTTP::Post.new("/v1.1/auth")
+    request = Net::HTTP::Post.new(uri.request_uri)
     request.add_field('Content-Type', 'application/x-www-form-urlencoded')
-    request.set_form_data({ ‘code’ => @code, ‘client_secret’ => "QKAURIDJ2E37OVWZOXMJLAMUGISHXLTLQR2IOBNNNTWAAU53BD" })
+    request.set_form_data({ 'code' => code, 'client_secret' => "QKAURIDJ2E37OVWZOXMJLAMUGISHXLTLQR2IOBNNNTWAAU53BD", 'grant_type' => 'authorization_code', 'client_id' => 'LM35T2HNQKLMFV5WVQ' })
     response = http.request(request)
-    puts response.body.read
+    puts "RESPONSE:"
+    puts response.code
+    puts response.body.class
+    resp = JSON.parse(response.body)
+    access_token = resp["access_token"]
+    puts "Token: "
+    puts access_token
+    
+    session[:eventbrite_auth_token] = access_token
+    redirect_to :controller => "home", :action => "index"
   end
 end
