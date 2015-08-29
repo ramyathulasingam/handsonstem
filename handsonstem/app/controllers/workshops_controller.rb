f0d81f233e1172c94dc14cc38724697798d07425
Bianca Tamayo <bancie@gmail.com>
2015-08-28 22:00:29 -0700

    Workshop/show view implemented and controller actions

--- handsonstem/app/controllers/workshops_controller.rb
+++ handsonstem/app/controllers/workshops_controller.rb
@@ -1,2 +1,14 @@
 class WorkshopsController < ApplicationController
+  def new
+    @workshop = Workshop.new
+  end
+  
+  def create
+    
+  end
+  
+  # GET /workshops/:id
+  def show
+    @workshop = Workshop.find(params[:id])
+  end
 end
